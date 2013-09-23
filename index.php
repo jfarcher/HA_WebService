ec49ab1ffaf8ab24679e49bec23d3132807e5fa4
Jonathan Archer <jfarchermobile@gmail.com>
2013-09-23 22:37:57 +0100

    initial modifications to reflect relevant paths.

--- index.php
+++ index.php
@@ -4,32 +4,23 @@
 <body>
 <?php
 require("config.php");
-require_once("phpMQTT/phpMQTT.php");
+require_once("../../phpMQTT/phpMQTT.php");
 
 $buttonId = $_REQUEST['buttonId'];
 //echo "b: $buttonId";
 
 
-
-switch( $device[$buttonId]['Type'])
-{
-	case "MQTT":
-		publish_to_mqtt( $device[$buttonId]['Topic'], $device[$buttonId]['Message'], $device[$buttonId]['Response'] );
-		break;
-	case "HTTP":
-		publish_to_http( $device[$buttonId]['URL'], $device[$buttonId]['Response'] );
-		break;
-}
-
+		
 /**
  */
 function publish_to_mqtt( $topic, $message, $response )
 {
 	global $config;
-	$mqtt = new phpMQTT($config['MQTTHost'], 1883, "PHP-MQTT-Client-".$_SERVER['SERVER_ADDR']);
+ 	$mqtt = new phpMQTT($config['MQTTHost'], 1883, "PHP-MQTT-Client-".$_SERVER['SERVER_ADDR']);
 	if( $mqtt->connect()) {
 		$mqtt->publish( $topic, $message );
-		$mqtt->close();
+	sleep(1);	
+	$mqtt->close();
 	}
 	echo $response;
 }
@@ -40,6 +31,15 @@ function publish_to_http( $url, $response )
 {
 	`curl -d $url`;
 	echo $response;
+}
+switch( $device[$buttonId]['Type'])
+{
+        case "MQTT":
+                publish_to_mqtt( $device[$buttonId]['Topic'], $device[$buttonId]['Message'], $device[$buttonId]['Response'] );
+                break;
+        case "HTTP":
+                publish_to_http( $device[$buttonId]['URL'], $device[$buttonId]['Response'] );
+                break;
 }
 
 ?>
